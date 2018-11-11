9e8fd0c86cefba16ff2d220101dc6c2b79de0687
melvinrr25 <melvinrr25@gmail.com>
2018-11-11 15:00:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/test', function(req, res){
-  res.json({author: 'melvinrr25@gmail.com || Melvin Rodriguez || Testing'});
+  res.json({author: 'melvinrr25@gmail.com'});
 });
 
 app.get('*', function(req, res){
